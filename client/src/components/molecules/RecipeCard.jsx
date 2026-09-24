5f5737911c989e770ae6b7a8fc52879fdf28d562
Yohanna Decilio <yohannadecilio@gmail.com>
2026-09-24 14:32:15 +0800

    updated RecipeCard.jsx to link the recipe details when clicked

--- client/src/components/molecules/RecipeCard.jsx
+++ client/src/components/molecules/RecipeCard.jsx
@@ -2,27 +2,27 @@ import { Link } from "react-router-dom";
 
 function RecipeCard({ recipe }) {
   return (
-    <article className="recipe-card">
-      {recipe.image_url ? (
-        <img
-          src={recipe.image_url}
-          alt={recipe.name}
-          className="recipe-image"
-        />
-      ) : (
-        <div className="recipe-image recipe-image-placeholder">
-          No image
-        </div>
-      )}
+    <Link
+      to={`/recipes/${recipe.id}`}
+      className="recipe-card-link"
+    >
+      <article className="recipe-card">
+        {recipe.image_url ? (
+          <img
+            src={recipe.image_url}
+            alt={recipe.name}
+            className="recipe-image"
+          />
+        ) : (
+          <div className="recipe-image recipe-image-placeholder">
+            No image
+          </div>
+        )}
 
-      <h3>{recipe.name}</h3>
-
-      <p>{recipe.category}</p>
-
-      <Link to={`/recipes/${recipe.id}`}>
-        View Recipe
-      </Link>
-    </article>
+        <h3>{recipe.name}</h3>
+        <p>{recipe.category}</p>
+      </article>
+    </Link>
   );
 }
 
